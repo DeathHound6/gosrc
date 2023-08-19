15e22b9b8b33b206544fdd5174c9bd1e0924835e
DeathHound <acraig176@hotmail.com>
2023-08-19 10:03:24 +0100

    Add GetCategory

--- v1/categories.go
+++ v1/categories.go
@@ -1,5 +1,13 @@
 package v1
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"gosrc"
+	"net/http"
+)
+
 type CategoryPlayers struct {
 	Type  string `json:"type"`
 	Value int    `json:"value"`
@@ -14,4 +22,45 @@ type Category struct {
 	Players       CategoryPlayers `json:"players"`
 	Miscellaneous bool            `json:"miscellaneous"`
 	Links         []Link          `json:"links"`
+}
+
+type CategoryResponse struct {
+	Data Category `json:"data"`
+}
+
+func GetCategory(id string) (*CategoryResponse, error) {
+	headers := map[string]string{
+		"Accept":       "application/json",
+		"Content-Type": "application/json",
+	}
+	jsonBody, err := json.Marshal(map[string]string{})
+	if err != nil {
+		return nil, err
+	}
+	reqBody := bytes.NewBuffer(jsonBody)
+	resp, err := gosrc.MakeRequest(APIVersion, fmt.Sprintf("categories/%s", id), http.MethodGet, headers, reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	bodyBytes := make([]byte, 0)
+	if _, err := resp.Body.Read(bodyBytes); err != nil {
+		return nil, err
+	}
+	if err := resp.Body.Close(); err != nil {
+		return nil, err
+	}
+	data := &CategoryResponse{}
+	if err := json.Unmarshal(bodyBytes, data); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func GetCategoryVariable(id string) []*Variable {
+
+}
+
+func GetCategoryRecords(id string) []*Leaderboard {
+
 }
