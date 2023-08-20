00494e075867d9e1f6cf8999a5c08b61b5587ed1
DeathHound <acraig176@hotmail.com>
2023-08-20 15:46:07 +0100

    Add PostRun

--- v1/runs.go
+++ v1/runs.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"gosrc"
 	"net/http"
@@ -66,12 +67,58 @@ type Run struct {
 	Links     []*Link           `json:"links"`
 }
 
+type PostRunBodyRunTimes struct {
+	Realtime        int `json:"realtime"`
+	RealtimeNoloads int `json:"realtime_noloads"`
+	Ingame          int `json:"ingame"`
+}
+
+type PostRunBodyPlayers struct {
+	Rel  string `json:"rel"`
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type PostRunBodyVariable struct {
+	Type  string `json:"type"`
+	Value string `json:"value"`
+}
+
+type PostRunBodyRun struct {
+	Category string `json:"category"`
+	Level    string `json:"level"`
+	Date     string `json:"date"`
+	Region   string `json:"region"`
+	Platform string `json:"platform"`
+	// This can only be set if the Authenticated user is a Moderator for the game
+	Verified  bool                            `json:"verified"`
+	Times     *PostRunBodyRunTimes            `json:"times"`
+	Players   []*PostRunBodyPlayers           `json:"players"`
+	Emulated  bool                            `json:"emulated"`
+	Video     string                          `json:"video"`
+	Comment   string                          `json:"comment"`
+	Splitsio  string                          `json:"splitsio"`
+	Variables map[string]*PostRunBodyVariable `json:"variables"`
+}
+
+type PostRunBody struct {
+	Run *PostRunBodyRun `json:"run"`
+}
+
 type RunsResponse struct {
 	Data []*Run `json:"data"`
 }
 
 type RunResponse struct {
 	Data *Run `json:"data"`
+}
+
+type PostRunErrorResponse struct {
+	error
+	Status  int      `json:"status"`
+	Message string   `json:"message"`
+	Errors  []string `json:"errors"`
+	Links   []*Link  `json:"links"`
 }
 
 func GetRuns() (*RunsResponse, error) {
@@ -130,4 +177,43 @@ func GetRun(runId string) (*RunResponse, error) {
 		return nil, err
 	}
 	return data, nil
+}
+
+// This endpoint requires Authentication
+func PostRun(body *PostRunBody) (*RunResponse, error) {
+	headers := map[string]string{
+		"Accept":       "application/json",
+		"Content-Type": "application/json",
+	}
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+	reqBody := bytes.NewBuffer(jsonBody)
+	resp, err := gosrc.MakeRequest(APIVersion, "runs", http.MethodPost, headers, reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	bodyBytes := make([]byte, 0)
+	if _, err := resp.Body.Read(bodyBytes); err != nil {
+		return nil, err
+	}
+	if err := resp.Body.Close(); err != nil {
+		return nil, err
+	}
+	if resp.StatusCode == 201 {
+		data := new(RunResponse)
+		if err := json.Unmarshal(bodyBytes, &data); err != nil {
+			return nil, err
+		}
+		return data, nil
+	} else {
+		data := new(PostRunErrorResponse)
+		if err := json.Unmarshal(bodyBytes, &data); err != nil {
+			return nil, err
+		}
+		data.error = errors.New(data.Message)
+		return nil, data
+	}
 }
