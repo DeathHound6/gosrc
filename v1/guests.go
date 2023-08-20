99ab0d9dd7d308f958eafa202ed53cd13047a2ad
DeathHound <acraig176@hotmail.com>
2023-08-20 18:10:19 +0100

    Add GetGuest

--- v1/guests.go
+++ v1/guests.go
@@ -1,6 +1,47 @@
 package v1
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"gosrc"
+	"net/http"
+)
+
 type Guest struct {
 	Name  string  `json:"name"`
 	Links []*Link `json:"links"`
+}
+
+type GuestResponse struct {
+	Data *Guest `json:"data"`
+}
+
+func GetGuest(guestName string) (*GuestResponse, error) {
+	headers := map[string]string{
+		"Accept":       "application/json",
+		"Content-Type": "application/json",
+	}
+	jsonBody, err := json.Marshal(map[string]string{})
+	if err != nil {
+		return nil, err
+	}
+	reqBody := bytes.NewBuffer(jsonBody)
+	resp, err := gosrc.MakeRequest(APIVersion, fmt.Sprintf("guests/%s", guestName), http.MethodGet, headers, reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	bodyBytes := make([]byte, 0)
+	if _, err := resp.Body.Read(bodyBytes); err != nil {
+		return nil, err
+	}
+	if err := resp.Body.Close(); err != nil {
+		return nil, err
+	}
+	data := new(GuestResponse)
+	if err := json.Unmarshal(bodyBytes, &data); err != nil {
+		return nil, err
+	}
+	return data, nil
 }
