87d5a9ab70b89b83c116516f3176e94ba4bf59d9
DeathHound <acraig176@hotmail.com>
2023-08-20 18:00:01 +0100

    Add GetGenre

--- v1/genres.go
+++ v1/genres.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"gosrc"
 	"net/http"
 )
@@ -15,6 +16,10 @@ type Genre struct {
 
 type GenresResponse struct {
 	Data []*Genre `json:"data"`
+}
+
+type GenreResponse struct {
+	Data *Genre `json:"data"`
 }
 
 func GetGenres() (*GenresResponse, error) {
@@ -40,6 +45,35 @@ func GetGenres() (*GenresResponse, error) {
 		return nil, err
 	}
 	data := new(GenresResponse)
+	if err := json.Unmarshal(bodyBytes, &data); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func GetGenre(genreId string) (*GenreResponse, error) {
+	headers := map[string]string{
+		"Accept":       "application/json",
+		"Content-Type": "application/json",
+	}
+	jsonBody, err := json.Marshal(map[string]string{})
+	if err != nil {
+		return nil, err
+	}
+	reqBody := bytes.NewBuffer(jsonBody)
+	resp, err := gosrc.MakeRequest(APIVersion, fmt.Sprintf("genres/%s", genreId), http.MethodGet, headers, reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	bodyBytes := make([]byte, 0)
+	if _, err := resp.Body.Read(bodyBytes); err != nil {
+		return nil, err
+	}
+	if err := resp.Body.Close(); err != nil {
+		return nil, err
+	}
+	data := new(GenreResponse)
 	if err := json.Unmarshal(bodyBytes, &data); err != nil {
 		return nil, err
 	}
