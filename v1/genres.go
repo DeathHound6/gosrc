b9518cad58b396cf97f22a178268b81b845c21d3
DeathHound <acraig176@hotmail.com>
2023-08-20 17:58:26 +0100

    Add GetGenres

--- v1/genres.go
+++ v1/genres.go
@@ -1,7 +1,47 @@
 package v1
 
+import (
+	"bytes"
+	"encoding/json"
+	"gosrc"
+	"net/http"
+)
+
 type Genre struct {
 	ID    string  `json:"id"`
 	Name  string  `json:"name"`
 	Links []*Link `json:"links"`
+}
+
+type GenresResponse struct {
+	Data []*Genre `json:"data"`
+}
+
+func GetGenres() (*GenresResponse, error) {
+	headers := map[string]string{
+		"Accept":       "application/json",
+		"Content-Type": "application/json",
+	}
+	jsonBody, err := json.Marshal(map[string]string{})
+	if err != nil {
+		return nil, err
+	}
+	reqBody := bytes.NewBuffer(jsonBody)
+	resp, err := gosrc.MakeRequest(APIVersion, "genres", http.MethodGet, headers, reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	bodyBytes := make([]byte, 0)
+	if _, err := resp.Body.Read(bodyBytes); err != nil {
+		return nil, err
+	}
+	if err := resp.Body.Close(); err != nil {
+		return nil, err
+	}
+	data := new(GenresResponse)
+	if err := json.Unmarshal(bodyBytes, &data); err != nil {
+		return nil, err
+	}
+	return data, nil
 }
