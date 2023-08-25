bf0766e17fef110abee7cef9a5a6a43f1905f6e2
DeathHound <acraig176@hotmail.com>
2023-08-25 21:32:48 +0100

    Add GetGametype

--- v1/game_types.go
+++ v1/game_types.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/DeathHound6/gosrc"
 	"net/http"
 )
@@ -15,6 +16,10 @@ type Gametype struct {
 
 type GametypesResponse struct {
 	Data []*Gametype `json:"data"`
+}
+
+type GametypeResponse struct {
+	Data *Gametype `json:"data"`
 }
 
 func GetGametypes() (*GametypesResponse, error) {
@@ -40,6 +45,35 @@ func GetGametypes() (*GametypesResponse, error) {
 		return nil, err
 	}
 	data := new(GametypesResponse)
+	if err := json.Unmarshal(bodyBytes, &data); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func GetGametype(gametypeId string) (*GametypeResponse, error) {
+	headers := map[string]string{
+		"Accept":       "application/json",
+		"Content-Type": "application/json",
+	}
+	jsonBody, err := json.Marshal(map[string]string{})
+	if err != nil {
+		return nil, err
+	}
+	reqBody := bytes.NewBuffer(jsonBody)
+	resp, err := gosrc.MakeRequest(APIVersion, fmt.Sprintf("gametypes/%s", gametypeId), http.MethodGet, headers, reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	bodyBytes := make([]byte, 0)
+	if _, err := resp.Body.Read(bodyBytes); err != nil {
+		return nil, err
+	}
+	if err := resp.Body.Close(); err != nil {
+		return nil, err
+	}
+	data := new(GametypeResponse)
 	if err := json.Unmarshal(bodyBytes, &data); err != nil {
 		return nil, err
 	}
