180815c6295ce9b100588f713a1b1ac1bc5d1450
DeathHound <acraig176@hotmail.com>
2023-08-25 21:06:11 +0100

    Add GetGameVariables

--- v1/games.go
+++ v1/games.go
@@ -61,6 +61,10 @@ type GameLevelsResponse struct {
 	Data []*Level `json:"data"`
 }
 
+type GameVariablesResponse struct {
+	Data []*Variable `json:"data"`
+}
+
 func GetGames() (*GamesResponse, error) {
 	headers := map[string]string{
 		"Accept":       "application/json",
@@ -171,6 +175,35 @@ func GetGameLevels(gameId string) (*GameLevelsResponse, error) {
 		return nil, err
 	}
 	data := new(GameLevelsResponse)
+	if err := json.Unmarshal(bodyBytes, &data); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func GetGameVariables(gameId string) (*GameVariablesResponse, error) {
+	headers := map[string]string{
+		"Accept":       "application/json",
+		"Content-Type": "application/json",
+	}
+	jsonBody, err := json.Marshal(map[string]string{})
+	if err != nil {
+		return nil, err
+	}
+	reqBody := bytes.NewBuffer(jsonBody)
+	resp, err := gosrc.MakeRequest(APIVersion, fmt.Sprintf("games/%s/variables", gameId), http.MethodGet, headers, reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	bodyBytes := make([]byte, 0)
+	if _, err := resp.Body.Read(bodyBytes); err != nil {
+		return nil, err
+	}
+	if err := resp.Body.Close(); err != nil {
+		return nil, err
+	}
+	data := new(GameVariablesResponse)
 	if err := json.Unmarshal(bodyBytes, &data); err != nil {
 		return nil, err
 	}
