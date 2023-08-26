44671e0a22f60701e812ddcf3f1b3969fe2d2482
DeathHound <acraig176@hotmail.com>
2023-08-26 16:14:16 +0100

    Add GetPublisher

--- v1/publishers.go
+++ v1/publishers.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/DeathHound6/gosrc"
 	"net/http"
 )
@@ -15,6 +16,10 @@ type Publisher struct {
 
 type PublishersResponse struct {
 	Data []*Publisher `json:"data"`
+}
+
+type PublisherResponse struct {
+	Data *Publisher `json:"data"`
 }
 
 func GetPublishers() (*PublishersResponse, error) {
@@ -40,6 +45,35 @@ func GetPublishers() (*PublishersResponse, error) {
 		return nil, err
 	}
 	data := new(PublishersResponse)
+	if err := json.Unmarshal(bodyBytes, &data); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func GetPublisher(publisherId string) (*PublisherResponse, error) {
+	headers := map[string]string{
+		"Accept":       "application/json",
+		"Content-Type": "application/json",
+	}
+	jsonBody, err := json.Marshal(map[string]string{})
+	if err != nil {
+		return nil, err
+	}
+	reqBody := bytes.NewBuffer(jsonBody)
+	resp, err := gosrc.MakeRequest(APIVersion, fmt.Sprintf("publishers/%s", publisherId), http.MethodGet, headers, reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	bodyBytes := make([]byte, 0)
+	if _, err := resp.Body.Read(bodyBytes); err != nil {
+		return nil, err
+	}
+	if err := resp.Body.Close(); err != nil {
+		return nil, err
+	}
+	data := new(PublisherResponse)
 	if err := json.Unmarshal(bodyBytes, &data); err != nil {
 		return nil, err
 	}
