dc674f5776e2e475c5a923b4423356e98e2be58f
DeathHound <acraig176@hotmail.com>
2023-08-26 16:17:31 +0100

    Add GetPlatforms

---
+++ v1/platforms.go
@@ -0,0 +1,48 @@
+package v1
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/DeathHound6/gosrc"
+	"net/http"
+)
+
+type Platform struct {
+	ID       string  `json:"id"`
+	Name     string  `json:"name"`
+	Released int     `json:"released"`
+	Links    []*Link `json:"links"`
+}
+
+type PlatformsResponse struct {
+	Data []*Platform `json:"data"`
+}
+
+func GetPlatforms() (*PlatformsResponse, error) {
+	headers := map[string]string{
+		"Accept":       "application/json",
+		"Content-Type": "application/json",
+	}
+	jsonBody, err := json.Marshal(map[string]string{})
+	if err != nil {
+		return nil, err
+	}
+	reqBody := bytes.NewBuffer(jsonBody)
+	resp, err := gosrc.MakeRequest(APIVersion, "platforms", http.MethodGet, headers, reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	bodyBytes := make([]byte, 0)
+	if _, err := resp.Body.Read(bodyBytes); err != nil {
+		return nil, err
+	}
+	if err := resp.Body.Close(); err != nil {
+		return nil, err
+	}
+	data := new(PlatformsResponse)
+	if err := json.Unmarshal(bodyBytes, &data); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
