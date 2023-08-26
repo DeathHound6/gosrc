fb2e4298b167cb65016687b493c5b41e6e8180e7
DeathHound <acraig176@hotmail.com>
2023-08-26 16:18:29 +0100

    Add GetPlatform

--- v1/platforms.go
+++ v1/platforms.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/DeathHound6/gosrc"
 	"net/http"
 )
@@ -16,6 +17,10 @@ type Platform struct {
 
 type PlatformsResponse struct {
 	Data []*Platform `json:"data"`
+}
+
+type PlatformResponse struct {
+	Data *Platform `json:"data"`
 }
 
 func GetPlatforms() (*PlatformsResponse, error) {
@@ -41,6 +46,35 @@ func GetPlatforms() (*PlatformsResponse, error) {
 		return nil, err
 	}
 	data := new(PlatformsResponse)
+	if err := json.Unmarshal(bodyBytes, &data); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func GetPlatform(platformId string) (*PlatformResponse, error) {
+	headers := map[string]string{
+		"Accept":       "application/json",
+		"Content-Type": "application/json",
+	}
+	jsonBody, err := json.Marshal(map[string]string{})
+	if err != nil {
+		return nil, err
+	}
+	reqBody := bytes.NewBuffer(jsonBody)
+	resp, err := gosrc.MakeRequest(APIVersion, fmt.Sprintf("platforms/%s", platformId), http.MethodGet, headers, reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	bodyBytes := make([]byte, 0)
+	if _, err := resp.Body.Read(bodyBytes); err != nil {
+		return nil, err
+	}
+	if err := resp.Body.Close(); err != nil {
+		return nil, err
+	}
+	data := new(PlatformResponse)
 	if err := json.Unmarshal(bodyBytes, &data); err != nil {
 		return nil, err
 	}
