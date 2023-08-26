3efb38c70010b7659aaa472f59e63339299df996
DeathHound <acraig176@hotmail.com>
2023-08-26 16:04:05 +0100

    Add GetSeries

--- v1/series.go
+++ v1/series.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/DeathHound6/gosrc"
 	"net/http"
 )
@@ -25,6 +26,10 @@ type Series struct {
 
 type SeriessResponse struct {
 	Data []*Series `json:"data"`
+}
+
+type SeriesResponse struct {
+	Data *Series `json:"data"`
 }
 
 func GetSeriess() (*SeriessResponse, error) {
@@ -50,6 +55,35 @@ func GetSeriess() (*SeriessResponse, error) {
 		return nil, err
 	}
 	data := new(SeriessResponse)
+	if err := json.Unmarshal(bodyBytes, &data); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func GetSeries(seriesId string) (*SeriesResponse, error) {
+	headers := map[string]string{
+		"Accept":       "application/json",
+		"Content-Type": "application/json",
+	}
+	jsonBody, err := json.Marshal(map[string]string{})
+	if err != nil {
+		return nil, err
+	}
+	reqBody := bytes.NewBuffer(jsonBody)
+	resp, err := gosrc.MakeRequest(APIVersion, fmt.Sprintf("series/%s", seriesId), http.MethodGet, headers, reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	bodyBytes := make([]byte, 0)
+	if _, err := resp.Body.Read(bodyBytes); err != nil {
+		return nil, err
+	}
+	if err := resp.Body.Close(); err != nil {
+		return nil, err
+	}
+	data := new(SeriesResponse)
 	if err := json.Unmarshal(bodyBytes, &data); err != nil {
 		return nil, err
 	}
