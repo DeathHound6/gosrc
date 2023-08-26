ed9c1278c5f186711b0f6c89a724e338708a98d5
DeathHound <acraig176@hotmail.com>
2023-08-26 16:09:00 +0100

    Add GetRegion

--- v1/regions.go
+++ v1/regions.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/DeathHound6/gosrc"
 	"net/http"
 )
@@ -15,6 +16,10 @@ type Region struct {
 
 type RegionsResponse struct {
 	Data []*Region `json:"data"`
+}
+
+type RegionResponse struct {
+	Data *Region `json:"data"`
 }
 
 func GetRegions() (*RegionsResponse, error) {
@@ -40,6 +45,35 @@ func GetRegions() (*RegionsResponse, error) {
 		return nil, err
 	}
 	data := new(RegionsResponse)
+	if err := json.Unmarshal(bodyBytes, &data); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func GetRegion(regionId string) (*RegionResponse, error) {
+	headers := map[string]string{
+		"Accept":       "application/json",
+		"Content-Type": "application/json",
+	}
+	jsonBody, err := json.Marshal(map[string]string{})
+	if err != nil {
+		return nil, err
+	}
+	reqBody := bytes.NewBuffer(jsonBody)
+	resp, err := gosrc.MakeRequest(APIVersion, fmt.Sprintf("regions", regionId), http.MethodGet, headers, reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	bodyBytes := make([]byte, 0)
+	if _, err := resp.Body.Read(bodyBytes); err != nil {
+		return nil, err
+	}
+	if err := resp.Body.Close(); err != nil {
+		return nil, err
+	}
+	data := new(RegionResponse)
 	if err := json.Unmarshal(bodyBytes, &data); err != nil {
 		return nil, err
 	}
