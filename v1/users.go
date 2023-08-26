343d1208364222e4252c0d11b3a74593e74fd24b
DeathHound <acraig176@hotmail.com>
2023-08-26 15:52:35 +0100

    Add GetUser

--- v1/users.go
+++ v1/users.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/DeathHound6/gosrc"
 	"net/http"
 )
@@ -56,6 +57,10 @@ type UsersResponse struct {
 	Data []*User `json:"data"`
 }
 
+type UserResponse struct {
+	Data *User `json:"data"`
+}
+
 func GetUsers() (*UsersResponse, error) {
 	headers := map[string]string{
 		"Accept":       "application/json",
@@ -79,6 +84,35 @@ func GetUsers() (*UsersResponse, error) {
 		return nil, err
 	}
 	data := new(UsersResponse)
+	if err := json.Unmarshal(bodyBytes, &data); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func GetUser(userId string) (*UserResponse, error) {
+	headers := map[string]string{
+		"Accept":       "application/json",
+		"Content-Type": "application/json",
+	}
+	jsonBody, err := json.Marshal(map[string]string{})
+	if err != nil {
+		return nil, err
+	}
+	reqBody := bytes.NewBuffer(jsonBody)
+	resp, err := gosrc.MakeRequest(APIVersion, fmt.Sprintf("users/%s", userId), http.MethodGet, headers, reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	bodyBytes := make([]byte, 0)
+	if _, err := resp.Body.Read(bodyBytes); err != nil {
+		return nil, err
+	}
+	if err := resp.Body.Close(); err != nil {
+		return nil, err
+	}
+	data := new(UserResponse)
 	if err := json.Unmarshal(bodyBytes, &data); err != nil {
 		return nil, err
 	}
